64818b8f8db7dca4c3bae33ab0897dc7b409647c
JamesCroissant <yuham1014@gmail.com>
2022-04-02 13:27:10 +0900

    ユーザー情報に関するバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は半角英数字を両方含める必要があります' }
+
   validates :family_name, presence: true
   validates :first_name, presence: true
   validates :furigana_family_name, presence: true
