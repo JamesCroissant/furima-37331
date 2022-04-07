a1c8e7c5e6f1b6be77e025aa58d47c02c7fe2ca7
JamesCroissant <yuham1014@gmail.com>
2022-04-07 14:21:32 +0900

    バリデーションの変更
    重複したエラーメッセージの削除とimageカラムのバリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :image,        presence: true
   validates :title,        presence: true
   validates :description,  presence: true
   validates :category_id,   numericality: { other_than: 1, message: "can't be blank" }
@@ -16,6 +17,15 @@ class Item < ApplicationRecord
   validates :delivery_charge_id,  numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_area_id,  numericality: { other_than: 1, message: "can't be blank" }
   validates :day_id,  numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999},
-             format: { with: /\A[0-9]+\z/ }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}, format: { with: /\A[0-9]+\z/} , allow_blank: true
+  validates :price, presence: true
+    
+
+  def self.search(search)
+    if search!=""
+      Item.where('text LIKE(?)', "%#{search}%")
+    else
+      Item.all
+    end
+  end
 end
