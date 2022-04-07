c006222bd306c01a02ab61632a1d16d5cda39173
JamesCroissant <yuham1014@gmail.com>
2022-04-07 14:34:10 +0900

    テストコードの実装変更
    itemがuserと紐づいているかどうかをテストするためのコードを実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -85,6 +85,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Day can't be blank")
       end
 
+      it 'userが紐づいていないと出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+
 
 
     end
