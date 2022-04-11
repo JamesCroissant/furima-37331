63ee503eca1238d899b68755723bf6074f1570bc
JamesCroissant <yuham1014@gmail.com>
2022-04-11 16:30:16 +0900

    updateアクションをコントローラーに定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
+
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -26,6 +27,14 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
   
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
 
   private
 
