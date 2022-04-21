752596973bd3c32fb555dc3992ccd787a7fee532
JamesCroissant <yuham1014@gmail.com>
2022-04-21 16:22:35 +0900

    order_addressモデルを作成
    formオブジェクトを利用

---
+++ app/models/order_address.rb
@@ -0,0 +1,20 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :post_code, :prefecture_id, :local_goverment, :house_number, :building, :phone_number, :user_id, :item_id, :token
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :local_goverment
+    validates :house_number
+    validates :phone_number
+    validates :token
+  end
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, local_goverment: local_goverment, house_number: house_number, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
