c8e52dbf33a7b1c00867aa5e6cab77fbf94c3747
JamesCroissant <yuham1014@gmail.com>
2022-04-22 20:18:33 +0900

    公開鍵、秘密鍵を環境変数へ移動

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,13 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
     redirect_to root_path if @item.user_id == current_user.id || @item.order.present?
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
@@ -27,8 +26,12 @@ class OrdersController < ApplicationController
     )
   end
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def pay_item
-    Payjp.api_key = 'sk_test_93d0bbe0271d06f1d6e3361f'
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
       card: order_params[:token],
